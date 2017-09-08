5e8be084e1d73b082d423a1a88b5ccb440769476
anitarwt <rwtanita@gmail.com>
2017-09-08 16:31:38 +0700

    config recaptcha

---
+++ application/config/recaptcha.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// To use reCAPTCHA, you need to sign up for an API key pair for your site.
+// link: http://www.google.com/recaptcha/admin
+$config['recaptcha_site_key'] = '6Lc38C8UAAAAAKBMnWBGn3wSoE652dx0yAYKR2GN';
+$config['recaptcha_secret_key'] = '6Lc38C8UAAAAABDvQB_3FM5ylZtXh_CGZPhdxQWQ';
+
+// reCAPTCHA supported 40+ languages listed here:
+// https://developers.google.com/recaptcha/docs/language
+$config['recaptcha_lang'] = 'en';
+
+/* End of file recaptcha.php */
+/* Location: ./application/config/recaptcha.php */
